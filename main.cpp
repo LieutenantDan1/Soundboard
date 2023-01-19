b1aab2154a7a6b69fb7ef7781eadf07667d48c77
Darek <darek.chominski@mail.utoronto.ca>
2023-01-19 11:45:26 -0500

    added streamed sounds for large files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+// #include <fstream>
 #include <iostream>
 #include <list>
 #include <memory>
@@ -10,6 +11,7 @@
 
 namespace fs = std::filesystem;
 
+const size_t STREAM_THRESHOLD = 1048576;
 const size_t MAX_POLYPHONY = 256;
 const std::string KEYS = 
     "`1234567890-=qwe"
@@ -19,7 +21,114 @@ const std::string KEYS =
     "RTYUIOP{}|ASDFGH"
     "JKL:\"ZXCVBNM<>?";
 
-std::unordered_map<char, sf::SoundBuffer> load_sounds()
+class SoundInstance
+{
+public:
+    SoundInstance()
+    {}
+    SoundInstance(SoundInstance&&) = delete;
+    SoundInstance(const SoundInstance&) = delete;
+    virtual ~SoundInstance()
+    {}
+    
+    virtual bool is_playing()
+    {
+        return false;
+    }
+};
+
+class StreamedSoundInstance : public SoundInstance
+{
+private:
+    sf::Music _music;
+
+public:
+    StreamedSoundInstance(const std::string& filename)
+    {
+        _music.openFromFile(filename);
+        _music.play();
+    }
+
+    bool is_playing() override
+    {
+        return _music.getStatus() == sf::Music::Playing;
+    }
+};
+
+class MemorySoundInstance : public SoundInstance
+{
+private:
+    sf::Sound _sound;
+    
+public:
+    MemorySoundInstance(const sf::SoundBuffer& buffer) : _sound(buffer)
+    {
+        _sound.play();
+    }
+
+    bool is_playing() override
+    {
+        return _sound.getStatus() == sf::Sound::Playing;
+    }
+};
+
+class SoundClip
+{
+public:
+    SoundClip()
+    {}
+    SoundClip(SoundClip&&) = delete;
+    SoundClip(const SoundClip&) = delete;
+    virtual ~SoundClip()
+    {}
+
+    virtual void play(std::list<std::unique_ptr<SoundInstance>>& pool) const
+    {}
+};
+
+class StreamedSoundClip : public SoundClip
+{
+private:
+    std::string _filename;
+
+public:
+    StreamedSoundClip(const std::string& filename) : _filename(filename)
+    {}
+
+    void play(std::list<std::unique_ptr<SoundInstance>>& pool) const override
+    {
+        pool.emplace_back(new StreamedSoundInstance(_filename));
+    }
+};
+
+class MemorySoundClip : public SoundClip
+{
+private:
+    bool _valid;
+    sf::SoundBuffer _buffer;
+
+public:
+    MemorySoundClip(const std::string& filename)
+    {
+        _valid = _buffer.loadFromFile(filename);
+    }
+
+    void play(std::list<std::unique_ptr<SoundInstance>>& pool) const override
+    {
+        if (_valid)
+            pool.emplace_back(new MemorySoundInstance(_buffer));
+    }
+};
+
+SoundClip* load_sound(const fs::path& filename)
+{
+    if (fs::file_size(filename) > STREAM_THRESHOLD)
+        return new StreamedSoundClip(filename);
+    else
+        return new MemorySoundClip(filename);
+}
+
+std::unordered_map<char, std::unique_ptr<SoundClip>> load_sounds()
 {
     fs::path path = utils::get_home_dir();
     path /= ".sb";
@@ -37,19 +146,14 @@ std::unordered_map<char, sf::SoundBuffer> load_sounds()
     }
 
     size_t key = 0;
-    std::unordered_map<char, sf::SoundBuffer> sounds;
+    std::unordered_map<char, std::unique_ptr<SoundClip>> sounds;
     for (const auto& entry: fs::recursive_directory_iterator(path))
     {
         if (key == KEYS.size())
             break;
-        auto it = sounds.emplace(KEYS[key], sf::SoundBuffer()).first;
-        if (!it->second.loadFromFile(entry.path()))
-            sounds.erase(it);
-        else
-        {
-            std::cout << KEYS[key] << ": " << entry.path().stem().string() << '\n';
-            ++key;
-        }
+        auto it = sounds.emplace(KEYS[key], load_sound(entry.path())).first;
+        std::cout << KEYS[key] << ": " << entry.path().stem().string() << '\n';
+        ++key;
     }
  
     return sounds;
@@ -57,17 +161,17 @@ std::unordered_map<char, sf::SoundBuffer> load_sounds()
 
 int main()
 {
-    std::unordered_map<char, sf::SoundBuffer> sounds = load_sounds();
+    std::unordered_map<char, std::unique_ptr<SoundClip>> sounds = load_sounds();
     if (sounds.empty())
         return 0;
-    std::list<sf::Sound> pool;
+    std::list<std::unique_ptr<SoundInstance>> pool;
 
     while (true)
     {
         char input = utils::getch();
         for (auto it = pool.begin(); it != pool.end();)
         {
-            if (it->getStatus() != sf::Sound::Playing)
+            if (!(*it)->is_playing())
                 it = pool.erase(it);
             else
                 ++it;
@@ -87,7 +191,7 @@ int main()
         {
             auto def = sounds.find(input);
             if (def != sounds.end() && pool.size() < MAX_POLYPHONY)
-                pool.emplace_back(def->second).play();
+                def->second->play(pool);
         }
         }
     }
